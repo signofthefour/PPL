c90f83820d049e88957fe837c71d9543a3eea5c5
Nguyen Tan Dat <dat.nguyen_bk@hcmut.edu.vn>
2020-09-26 20:33:10 +0700

    INITIAL: init

---
+++ LexicalAnalysis/LexicalAnalysis.g4
@@ -0,0 +1,6 @@
+grammar LexicalAnalysis;
+
+fragment Letter: [a-z];
+fragment Number: [0-9];
+
+ManyLetter: Letter+;
---
+++ setup.sh
@@ -0,0 +1,3 @@
+export CLASSPATH=".:/home/nguyendat/Documents/projects/PPL/antlr-4.8-complete.jar:$CLASSPATH"
+alias antlr4='java -Xmx500M -cp "/home/nguyendat/Documents/projects/PPL/antlr-4.8-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
+alias grun='java -Xmx500M -cp "/home/nguyendat/Documents/projects/PPL/antlr-4.8-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
